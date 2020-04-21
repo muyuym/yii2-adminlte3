17239ce6cb59980ebab312120eb4f19b83be20d3
hail <hail812@163.com>
2020-04-21 11:44:04 +0800

    Callout

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -5,6 +5,11 @@
                 'type' => 'success',
                 'body' => '<h3>Congratulations!</h3>',
             ]) ?>
+            <?= \hail812\adminlte3\widgets\Callout::widget([
+                'type' => 'danger',
+                'head' => 'I am a danger callout!',
+                'body' => 'There is a problem that we need to fix. A wonderful serenity has taken possession of my entire soul, like these sweet mornings of spring which I enjoy with my whole heart.'
+            ]) ?>
         </div>
     </div>
 </div>
---
+++ src/widgets/Callout.php
@@ -0,0 +1,79 @@
+<?php
+namespace hail812\adminlte3\widgets;
+
+use yii\base\ErrorException;
+use yii\bootstrap4\Widget;
+use yii\helpers\Html;
+
+/**
+ * Class Callout
+ * @package hail812\adminlte3\widgets
+ * @example
+ * <?= Callout::widget(['type'=>'info', 'head'=>'head string', 'body'=>'body string']) ?>
+ * Also possible
+ * <?php Callout::begin(['type'=>'info', 'head'=>'head string']) ?>
+ *      body content
+ * <?php Callout::end() ?>
+ */
+class Callout extends Widget
+{
+    /**
+     * @var array supported type
+     */
+    public $supportedType = ['danger', 'info', 'warning', 'success'];
+
+    public $type;
+
+    /**
+     * @var string
+     */
+    public $head;
+
+    /**
+     * @var string
+     */
+    public $body;
+
+    /**
+     * @inheritdoc
+     */
+    public $options = [];
+
+    /**
+     * @var string $template
+     */
+    public $template = <<<html
+<div {options}>
+    <h5>{head}</h5>
+    <p>{body}</p>
+</div>
+html;
+
+    public function init()
+    {
+        parent::init();
+
+        if (is_null($this->type)) {
+            $this->type = 'info';
+        }
+        if (!in_array($this->type, $this->supportedType)) {
+            throw new ErrorException('unsupported type: '.$this->type);
+        }
+
+        Html::addCssClass($this->options, 'callout');
+        Html::addCssClass($this->options, 'callout-'.$this->type);
+
+        ob_start();
+    }
+
+    public function run()
+    {
+        $content = ob_get_clean();
+
+        return strtr($this->template, [
+            '{options}' => Html::renderTagAttributes($this->options),
+            '{head}' => $this->head,
+            '{body}' => $this->body.$content
+        ]);
+    }
+}
