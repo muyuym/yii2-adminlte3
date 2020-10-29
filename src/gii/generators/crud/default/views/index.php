bc117ca2d36672a66e7766f375c7f5a3e9480f84
hail <hail812@163.com>
2020-10-29 17:02:05 +0800

    crud

--- src/gii/generators/crud/default/views/index.php
+++ src/gii/generators/crud/default/views/index.php
@@ -51,18 +51,18 @@ $count = 0;
 if (($tableSchema = $generator->getTableSchema()) === false) {
     foreach ($generator->getColumnNames() as $name) {
         if (++$count < 6) {
-            echo "                          '" . $name . "',\n";
+            echo "                            '" . $name . "',\n";
         } else {
-            echo "                          //'" . $name . "',\n";
+            echo "                            //'" . $name . "',\n";
         }
     }
 } else {
     foreach ($tableSchema->columns as $column) {
         $format = $generator->generateColumnFormat($column);
         if (++$count < 6) {
-            echo "                          '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+            echo "                            '" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
         } else {
-            echo "                          //'" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
+            echo "                            //'" . $column->name . ($format === 'text' ? "" : ":" . $format) . "',\n";
         }
     }
 }
