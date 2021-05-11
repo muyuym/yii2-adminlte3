aad4153267824bfff00b236cdf8bc43538c4311d
hail <hail812@163.com>
2021-05-11 15:27:41 +0800

    views/site/index.php

--- src/views/site/index.php
+++ src/views/site/index.php
@@ -16,4 +16,109 @@ $this->params['breadcrumbs'] = [['label' => $this->title]];
             ]) ?>
         </div>
     </div>
+
+    <div class="row">
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Messages',
+                'number' => '1,410',
+                'icon' => 'far fa-envelope',
+            ]) ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Bookmarks',
+                'number' => '410',
+                 'theme' => 'success',
+                'icon' => 'far fa-flag',
+            ]) ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Uploads',
+                'number' => '13,648',
+                'theme' => 'gradient-warning',
+                'icon' => 'far fa-copy',
+            ]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Bookmarks',
+                'number' => '41,410',
+                'icon' => 'far fa-bookmark',
+                'progress' => [
+                    'width' => '70%',
+                    'description' => '70% Increase in 30 Days'
+                ]
+            ]) ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?php $infoBox = \hail812\adminlte\widgets\InfoBox::begin([
+                'text' => 'Likes',
+                'number' => '41,410',
+                'theme' => 'success',
+                'icon' => 'far fa-thumbs-up',
+                'progress' => [
+                    'width' => '70%',
+                    'description' => '70% Increase in 30 Days'
+                ]
+            ]) ?>
+            <?= \hail812\adminlte\widgets\Ribbon::widget([
+                'id' => $infoBox->id.'-ribbon',
+                'text' => 'Ribbon',
+            ]) ?>
+            <?php \hail812\adminlte\widgets\InfoBox::end() ?>
+        </div>
+        <div class="col-md-4 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\InfoBox::widget([
+                'text' => 'Events',
+                'number' => '41,410',
+                'theme' => 'gradient-warning',
+                'icon' => 'far fa-calendar-alt',
+                'progress' => [
+                    'width' => '70%',
+                    'description' => '70% Increase in 30 Days'
+                ],
+                'loadingStyle' => true
+            ]) ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\SmallBox::widget([
+                'title' => '150',
+                'text' => 'New Orders',
+                'icon' => 'fas fa-shopping-cart',
+            ]) ?>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <?php $smallBox = \hail812\adminlte\widgets\SmallBox::begin([
+                'title' => '150',
+                'text' => 'New Orders',
+                'icon' => 'fas fa-shopping-cart',
+                'theme' => 'success'
+            ]) ?>
+            <?= \hail812\adminlte\widgets\Ribbon::widget([
+                'id' => $smallBox->id.'-ribbon',
+                'text' => 'Ribbon',
+                'theme' => 'warning',
+                'size' => 'lg',
+                'textSize' => 'lg'
+            ]) ?>
+            <?php \hail812\adminlte\widgets\SmallBox::end() ?>
+        </div>
+        <div class="col-lg-4 col-md-6 col-sm-6 col-12">
+            <?= \hail812\adminlte\widgets\SmallBox::widget([
+                'title' => '44',
+                'text' => 'User Registrations',
+                'icon' => 'fas fa-user-plus',
+                'theme' => 'gradient-success',
+                'loadingStyle' => true
+            ]) ?>
+        </div>
+    </div>
 </div>
